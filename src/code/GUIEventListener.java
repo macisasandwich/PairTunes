3ba9d47b54f2d5fbcaff9c879cc053a8e80ee25f
sullstecclub <theboss@wirelessprvnat-172-16-138-68.near.illinois.edu>
2013-09-06 19:47:03 -0500

    Added ability to choose directory with JFileChooser.

--- src/code/GUIEventListener.java
+++ src/code/GUIEventListener.java
@@ -4,6 +4,9 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.Map;
 
+import javax.swing.JFileChooser;
+import javax.swing.JTextField;
+
 public class GUIEventListener implements ActionListener {
 
 	Map<String, Object> eventSources;
@@ -15,8 +18,14 @@ public class GUIEventListener implements ActionListener {
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		if(e.getSource() == eventSources.get("loadButton")) {
-			System.out.println("yeah we got dis shiz");
+			JFileChooser fileChooser = new JFileChooser();
+			fileChooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+			
+			int returnVal = fileChooser.showOpenDialog(fileChooser);
+
+	        if (returnVal == JFileChooser.APPROVE_OPTION) {							//Actually gives us the current directory
+	            ((JTextField) (eventSources.get("displaySong"))).setText(fileChooser.getSelectedFile().toString());
+	        }
 		}
 	}
-
 }
--- src/code/Main.java
+++ src/code/Main.java
@@ -3,7 +3,7 @@ package code;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+		System.out.println("PairTunes Running...");
 		GUIEventListener guiEventListener = new GUIEventListener(); //Create event listener
 		Window mainWindow = new Window(guiEventListener); //Make window with the listener
 		guiEventListener.setSources(mainWindow.getSources()); //Set the event listner's list of event sources from the window
