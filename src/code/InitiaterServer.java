ec95ec10a5452c83c7a0718f98e9da58a55c1a31
sullstecclub <sullstecclub@gmail.com>
2013-09-07 02:47:50 -0500

    Update InitiaterServer.java

--- src/code/InitiaterServer.java
+++ src/code/InitiaterServer.java
@@ -2,7 +2,6 @@ package code;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -13,11 +12,14 @@ public class InitiaterServer {
 		try {
 			server = new ServerSocket(42050);
 			client = server.accept();
-			PrintWriter out = new PrintWriter(client.getOutputStream(), true);
+//			/sdlfjaksdf
+			//PrintWriter out = new PrintWriter(client.getOutputStream(), true);
 			BufferedReader in = new BufferedReader(new InputStreamReader(client.getInputStream()));
-			out.print("go");
+			in.readLine();
+			RTPClient.entry("172.16.138.68");
 		} catch (Exception x) {
 			x.printStackTrace();
 		}
 	}
 }
+
