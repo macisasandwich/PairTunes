1f592ec8df8f19b05f452b96b7d3cb1b052c09f8
shunfu <shunhua.fu@gmail.com>
2013-09-07 11:57:22 -0500

    Moved the MouseListeners to the GUIEventListener class.

--- src/code/GUIEventListener.java
+++ src/code/GUIEventListener.java
@@ -1,7 +1,11 @@
 package code;
 
+import java.awt.Rectangle;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.net.URL;
 import java.util.Map;
 
@@ -50,6 +54,37 @@ public class GUIEventListener implements ActionListener, ControllerListener {
 	            		window.songListModel.remove(0);
 	            	window.songListModel.addElement(file.getName());
 	            }
+	            
+	            //Set mouse listener for songList
+	    		MouseListener mouseListener = new MouseAdapter() {
+	    		    public void mouseClicked(MouseEvent e) {
+	    		    	Rectangle r = window.songList.getCellBounds(0, window.songList.getLastVisibleIndex());
+	    		    	if (r != null && r.contains(e.getPoint()) && e.getClickCount() == 2) {
+	    		    		int index = window.songList.locationToIndex(e.getPoint());
+	    		    		//edge case: we only add to queue if it's not the default non-song message
+	    		    		if (!window.songListModel.getElementAt(index).equals("<Add some songs!>")) {
+	    		    			window.queueModel.addElement(window.songListModel.getElementAt(index));
+	    		    		}
+	    		    	}
+	    		    }
+	    		};
+	    		window.songList.addMouseListener(mouseListener);
+	    		
+	    		//Set mouse listener for queueList
+	    		MouseListener queueMouseListener = new MouseAdapter() {
+	    		    public void mouseClicked(MouseEvent e) {
+	    		    	Rectangle r = window.queueList.getCellBounds(0, window.queueList.getLastVisibleIndex());
+	    		    	if (r != null && r.contains(e.getPoint()) && e.getClickCount() == 2) {
+	    		    		int index = window.queueList.locationToIndex(e.getPoint());
+	    		    		//TODO actually start broadcasting
+	    	    			System.out.println("Playing: " + window.queueModel.getElementAt(index));
+	    		    	}
+	    		    }
+	    		};
+	    		window.queueList.addMouseListener(queueMouseListener);
+	    		
+	    		//TODO have the top of queue be playing
+	    		//TODO song list and queue are synced between comps
 
 //	            RTPServer.entry("172.16.138.68", "file:///C:\\Users\\JESSE\\Desktop\\Developer\\GitHub\\PairTunes\\src\\res\\17 Jeremy Soule - Secunda.wav");
 //	            RTPClient.entry("172.16.150.122");
@@ -58,11 +93,12 @@ public class GUIEventListener implements ActionListener, ControllerListener {
 	            
 	            /* Sam's IP = 172.16.200.239
 	             * Jesse's IP = 172.16.150.122
-	             * Stephen's IP = 172.16.???.???
+	             * Stephen's IP = 172.16.138.68
 	             */
 	            
 	            String samIP = "172.16.200.239";
 	            String jesseIP = "172.16.150.122";
+	            String stephenIP = "172.16.138.68";
 	            int port1 = 42050;
 	            int port2 = 25000;
 	            
