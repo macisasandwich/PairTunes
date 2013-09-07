68414fc27b55b0315c5541dd12a840bb4c8d43d2
sullstecclub <theboss@wirelessprvnat-172-16-138-68.near.illinois.edu>
2013-09-06 20:48:06 -0500

    Plays MUSIC BITCHES

--- src/code/GUIEventListener.java
+++ src/code/GUIEventListener.java
@@ -2,14 +2,20 @@ package code;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.net.URL;
 import java.util.Map;
 
+import javax.media.ControllerEvent;
+import javax.media.ControllerListener;
+import javax.media.Manager;
+import javax.media.Player;
 import javax.swing.JFileChooser;
 import javax.swing.JTextField;
 
-public class GUIEventListener implements ActionListener {
+public class GUIEventListener implements ActionListener, ControllerListener {
 
 	Map<String, Object> eventSources;
+	Player player;
 	
 	public void setSources(Map<String, Object> eventSources) {
 		this.eventSources = eventSources;
@@ -25,7 +31,20 @@ public class GUIEventListener implements ActionListener {
 
 	        if (returnVal == JFileChooser.APPROVE_OPTION) {							//Actually gives us the current directory
 	            ((JTextField) (eventSources.get("displaySong"))).setText(fileChooser.getSelectedFile().toString());
+	            try {
+	            	//Creates player, starts stream from file (plays song)
+					player = Manager.createPlayer(new URL("file:///Users/theboss/Music/test.wav"));
+					player.addControllerListener(this);
+					player.start();
+				} catch (Exception x) {
+					System.out.println(x);
+				}
 	        }
 		}
+	}
+
+	@Override
+	public void controllerUpdate(ControllerEvent arg0) {
+		
 	}
 }
