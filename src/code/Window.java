418e699278a229c365b59c1669b4f6f5ba53bde0
sullstecclub <theboss@wirelessprvnat-172-16-138-68.near.illinois.edu>
2013-09-07 00:48:38 -0500

    removed netnav

--- src/code/Window.java
+++ src/code/Window.java
@@ -1,11 +1,14 @@
 package code;
 
 import java.awt.BorderLayout;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 import java.util.HashMap;
 import java.util.Map;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
@@ -14,7 +17,8 @@ public class Window extends JFrame {
 	
 	Map<String, Object> eventSources;
 	JButton loadButton;
-	JTextField displaySong;
+	JTextField displaySong, friendIPField;
+	JLabel myIPLabel, friendIPLabel;
 	GUIEventListener eventListener;
 	
 	public Window(GUIEventListener listener) {
@@ -22,9 +26,27 @@ public class Window extends JFrame {
 		//Set up GUI components
 		loadButton = new JButton("Load...");
 		displaySong = new JTextField();
+		friendIPField = new JTextField(10);
+		friendIPLabel = new JLabel("Friend IP Address:");
+		String ip = "Error";
+		try {
+			ip = InetAddress.getLocalHost().getHostAddress();
+		} catch (UnknownHostException e) {
+			e.printStackTrace();
+		}
+		myIPLabel = new JLabel("My IP Address: "+ip);
+		
 		setLayout(new BorderLayout());
 		JPanel bottomPanel = new JPanel();
 		add(bottomPanel, BorderLayout.SOUTH);
+		JPanel topPanel = new JPanel();
+		add(topPanel, BorderLayout.NORTH);
+		JPanel topRightPanel = new JPanel();
+		
+		topPanel.add(myIPLabel);
+		topPanel.add(topRightPanel);
+		topRightPanel.add(friendIPLabel);
+		topRightPanel.add(friendIPField);
 		bottomPanel.setLayout(new BorderLayout());
 		bottomPanel.add(displaySong, BorderLayout.CENTER);
 		bottomPanel.add(loadButton, BorderLayout.EAST);
